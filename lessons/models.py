2bdb3655aa9e7eec47b3e33d98fab29250d3abf0
Matthew Kirk <matt@matthewkirk.com>
2017-10-25 13:17:36 -0700

    Add initial data model for lessons

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Lesson(models.Model):
+    scheduled_at    = models.DateTimeField()
+    created_at      = models.DateTimeField(auto_now_add=True)
+    updated_at      = models.DateTimeField(auto_now=True)
+    notes           = models.TextField()
+
+    # has many attachments
+    # has many teachers and students
+    # has many 
+
+class Attachment(models.Model):
+    url             = models.URLField()
+
+class HomeworkItem(models.Model):
+    name            = models.CharField(max_length=255, unique=True, blank=False)
+    proficiency     = models.IntegerField(default=1, validators=[MaxValueValidator(5), MinValueValidator(1)])
